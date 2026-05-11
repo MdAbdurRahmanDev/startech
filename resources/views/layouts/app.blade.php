0d137a54ecdf0e950c72d8696f5a3af9244770c9
Md Abdur Rahman <mdrahmanmiah2003@gmail.com>
2026-05-12 01:26:48 +0600

    feat: append site branding to page title in main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', $setting->app_name ?? 'Site') </title>
+    <title>@yield('title', $setting->app_name ?? 'Site') | International Office Solution</title>
 
     @if ($setting && $setting->favicon)
         <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . $setting->favicon) }}">
